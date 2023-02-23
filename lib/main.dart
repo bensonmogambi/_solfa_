c20f6cde333f32d597919e83faadcb5c12e6b678
Software Engineer <99333985+bensonmogambi@users.noreply.github.com>
2023-02-23 15:11:15 +0300

    few changes here too

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_solfapay_MVP-clone_ui/constant/Constant.dart';
+import 'package:flutter_solfapay_MVP-clone_uii/ui/solfapay.dart';
+import 'package:flutter_solfapay_MVP-clone_ui/ui/splashscreen.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+      theme: new ThemeData(primaryColor: Colors.blue[800],
+      ),
+      routes: <String, WidgetBuilder>{
+        SPLASH_SCREEN: (BuildContext context) => SplashScreen(),
+        SOLFA_PAY: (BuildContext context) => solfapay(),
+
+
+      },
+    );
+  }
+}
